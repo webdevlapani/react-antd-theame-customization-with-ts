84fcf2110a69f7f83370a739d0ca1dd1a1314240
slapani88 <webdev.lapani88@gmail.com>
2020-04-26 11:58:21 +0530

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import { Alert, Button } from 'antd';
 import React from 'react';
 import './App.css';
 
+interface ITest {
+  name: string;
+}
 const App = () => {
+  const data: ITest = { name: 'shiva' };
   return (
     <div>
-      <Button type="primary" disabled={true}>
+      <Button type="primary" disabled>
         Button
       </Button>
       <Button type="danger">Button</Button>
